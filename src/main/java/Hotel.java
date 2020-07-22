e93478211b8f7024f1d271f9dec6a89b2fb4f4b7
Rumen Roger <rumenroger@gmail.com>
2020-07-22 16:00:03 +0100

    Hotel Capacity

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,21 +1,74 @@
+import java.awt.print.Book;
+
+import java.awt.print.Book;import java.awt.print.Book;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Hotel {
 
     private ArrayList<Bedroom> bedrooms;
     private ArrayList<ConferenceRoom> conferenceRooms;
 
-    public Hotel(ArrayList<Bedroom> bedrooms, ArrayList<ConferenceRoom> conferenceRooms){
+    public Hotel(ArrayList<Bedroom> bedrooms, ArrayList<ConferenceRoom> conferenceRooms) {
         this.bedrooms = bedrooms;
         this.conferenceRooms = conferenceRooms;
     }
 
-    public boolean checkInGuestToBedroom(Guest guest, Bedroom bedroom){
+    public boolean checkInGuestToBedroom(Guest guest, Bedroom bedroom) {
         return bedroom.addGuest(guest);
     }
 
-    public boolean checkOutGuestFromBedroom(Guest guest, Bedroom bedroom){
+    public boolean checkOutGuestFromBedroom(Guest guest, Bedroom bedroom) {
         return bedroom.removeGuest(guest);
+    }
+
+    public HashMap<Integer, ArrayList<Bedroom>> getBedroomsGroupedByCapacity() {
+//       we declare a variable "bedroomCapacityHash" which expects a HashMap
+//       with an Integer as its keys, and an
+//       ArrayList of Bedroom objects as its values.
+            HashMap<Integer, ArrayList<Bedroom>> bedroomCapacityHash;
+//       we instantiate a HashMap which expects an Integer as its keys, and an
+//       ArrayList of Bedroom objects as its values.
+            bedroomCapacityHash = new  HashMap<Integer, ArrayList<Bedroom>>();
+            this.bedrooms.forEach( bedroom -> {
+                        if(!bedroomCapacityHash.containsKey(bedroom.getCapacity())) {
+                            ArrayList<Bedroom> capacityArrayListValue = new ArrayList<Bedroom>();
+                            capacityArrayListValue.add(bedroom);
+                            bedroomCapacityHash.put(bedroom.getCapacity(), capacityArrayListValue);
+                        }
+                        else {
+                            ArrayList<Bedroom> keyArrayListValue = bedroomCapacityHash.get(bedroom.getCapacity());
+                            keyArrayListValue.add(bedroom);
+                            bedroomCapacityHash.put(bedroom.getCapacity(), keyArrayListValue);
+                        }
+
+                    }
+            );
+              System.out.println(bedroomCapacityHash);
+
+            return bedroomCapacityHash;
+    }
+
+    public HashMap<Integer, ArrayList<Bedroom>> getBedroomsGroupedByCapacity2() {
+
+        HashMap<Integer, ArrayList<Bedroom>> bedroomCapacityHash;
+
+        bedroomCapacityHash = new HashMap<Integer, ArrayList<Bedroom>>();
+        this.bedrooms.forEach( bedroom -> {
+            int key = bedroom.getCapacity();
+            ArrayList<Bedroom> capacityArrayListValue = new ArrayList<Bedroom>();
+            capacityArrayListValue.add(bedroom);
+
+            bedroomCapacityHash.merge(key, capacityArrayListValue,(prev, room) ->
+                    prev.add(bedroom)
+
+            );
+
+                }
+        );
+        System.out.println(bedroomCapacityHash);
+
+        return bedroomCapacityHash;
     }
 
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -11,6 +11,7 @@ public class HotelTest {
     private Hotel hotel;
     private Bedroom bedroom1;
     private Bedroom bedroom2;
+    private Bedroom bedroom3;
     private ConferenceRoom conferenceRoom1;
     private ConferenceRoom conferenceRoom2;
     private Guest guest1;
@@ -23,9 +24,12 @@ public class HotelTest {
     public void before(){
         bedroom1 = new Bedroom(100, 4, 45.00);
         bedroom2 = new Bedroom(666, 2, 333.33);
+        bedroom3 = new Bedroom(866, 2, 333.33);
+
         ArrayList<Bedroom> bedrooms = new ArrayList<Bedroom>();
         bedrooms.add(bedroom1);
         bedrooms.add(bedroom2);
+        bedrooms.add(bedroom3);
 
         conferenceRoom1 = new ConferenceRoom("Melinda's Hall", 75);
         conferenceRoom2 = new ConferenceRoom("Superheroes Den", 3);
@@ -50,5 +54,15 @@ public class HotelTest {
     @Test
     public void canCheckOutGuestFromBedroom(){
         assertFalse(hotel.checkOutGuestFromBedroom(guest1, bedroom1));
+    }
+
+    @Test
+    public void canGetBedrooms() {
+        assertEquals(2, hotel.getBedroomsGroupedByCapacity().size());
+    }
+
+    @Test
+    public void canGetCapacityHash() {
+        assertEquals(2, hotel.getBedroomsGroupedByCapacity().size());
     }
 }
